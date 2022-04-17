7ab77d5d2874193046f50b712786ca6c427c2a1f
glaukiol1 <glaukiol1@gmail.com>
2022-04-18 01:29:57 +0200

    update seconds example

--- main.c
+++ main.c
@@ -7,9 +7,12 @@ struct data
 
 int main(int argc, char const *argv[])
 {
-   float data;
-   printf("Enter float: ");
-   scanf("%f", &data);
-   printf("Decimal: %f\n", data - ((int) data));
+   float seconds;
+   printf("Enter Seconds: ");
+   scanf("%f", &seconds);
+   int hours = seconds/3600;
+   int minutes = (seconds - (hours*3600)) / 60;
+   int cseconds = (int) (seconds - (hours*3600)) % 60;
+   printf("%02d:%02d:%02d\n", hours, minutes, cseconds);
    return 0;
 }
