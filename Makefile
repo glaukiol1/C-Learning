389568973daa9cdd5278cabf68b5046ce9b497d4
glaukiol1 <glaukiol1@gmail.com>
2022-04-20 21:58:08 +0200

    1 cent doubles each day for 30 days

--- Makefile
+++ Makefile
@@ -3,5 +3,6 @@
 run:
 	make build
 	./main
+	rm main
 build:
 	gcc main.c -o main
--- main.c
+++ main.c
@@ -1,18 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
-
-struct data
-{
-    int world;
-};
 
 int main(int argc, char const *argv[])
 {
-   float seconds;
-   printf("Enter Seconds: ");
-   scanf("%f", &seconds);
-   int hours = seconds/3600;
-   int minutes = (seconds - (hours*3600)) / 60;
-   int cseconds = (int) (seconds - (hours*3600)) % 60;
-   printf("%02d:%02d:%02d\n", hours, minutes, cseconds);
-   return 0;
+    int n = 30;
+    int money = 1;
+
+    for (int i = 0; i < n; i++) {
+        printf("$%.2lf in day %d\n", money/100.00, i+1);
+        money = money * 2;
+    }
+    printf("Total amount after 30 days: $%.2lf\n", money/100.00);
+    return 0;
 }
