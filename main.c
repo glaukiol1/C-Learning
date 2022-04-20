359f8213d52868ddface9a6c91b76a99f27d0a29
glaukiol1 <glaukiol1@gmail.com>
2022-04-21 01:33:59 +0200

    simple command line GUI program

--- main.c
+++ main.c
@@ -1,15 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define width 168 // the width of the command line
+
+void printUp(int height)
+{
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            printf("=");
+        }
+        printf("\n");
+    }
+}
+
+void printSides(char *data)
+{
+    for (int i = 0; i < 14; i++)
+    {
+        if (i == 7)
+        {
+            printf("||||");
+            int b = 0;
+            for (size_t j = 0; j < (width / 2) - (strlen(data)); j++)
+            {
+                printf(" ");
+                b = b + 1;
+            }
+            printf("%s", data);
+            for (int j = 0; j < b + (int)(strlen(data)) - 8; j++)
+            {
+                printf(" ");
+            }
+            printf("||||");
+        }
+        else
+        {
+            printf("||||");
+            for (int j = 0; j < (int)(width - 8); j++)
+            {
+                printf(" ");
+            }
+            printf("||||");
+        }
+        printf("\n");
+    }
+}
+
+void printBottom(int height)
+{
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            printf("=");
+        }
+    }
+}
+
+void printScreen()
+{
+    printUp(2);
+    char literal[] = "hello world";
+    printSides(literal);
+    printBottom(2);
+}
 
 int main(int argc, char const *argv[])
 {
-    int n = 30;
-    int money = 1;
-
-    for (int i = 0; i < n; i++) {
-        printf("$%.2lf in day %d\n", money/100.00, i+1);
-        money = money * 2;
-    }
-    printf("Total amount after 30 days: $%.2lf\n", money/100.00);
+    printScreen();
     return 0;
 }
