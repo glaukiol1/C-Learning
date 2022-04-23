89ff3b15a4af1612038576af72ad1863a057e5ac
glaukiol1 <glaukiol1@gmail.com>
2022-04-23 15:12:03 +0200

    type assertion

--- main.c
+++ main.c
@@ -1,75 +1,31 @@
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+// #include <string.h>
 
-#define width 168 // the width of the command line
+#define myFunction(X) _Generic((X), \
+    int *:   myFunctionInt,         \
+    float *: myFunctionFloat,       \
+    default: myFunctionDefault      \
+)(X)
 
-void printUp(int height)
-{
-    for (int j = 0; j < height; j++)
-    {
-        for (int i = 0; i < width; i++)
-        {
-            printf("=");
-        }
-        printf("\n");
-    }
+void myFunctionInt(int *f) {
+    printf("myFunctionInt: %d\n", *f);
 }
 
-void printSides(char *data)
-{
-    for (int i = 0; i < 14; i++)
-    {
-        if (i == 7)
-        {
-            printf("||||");
-            int b = 0;
-            for (size_t j = 0; j < (width / 2) - (strlen(data)); j++)
-            {
-                printf(" ");
-                b = b + 1;
-            }
-            printf("%s", data);
-            for (int j = 0; j < b + (int)(strlen(data)) - 8; j++)
-            {
-                printf(" ");
-            }
-            printf("||||");
-        }
-        else
-        {
-            printf("||||");
-            for (int j = 0; j < (int)(width - 8); j++)
-            {
-                printf(" ");
-            }
-            printf("||||");
-        }
-        printf("\n");
-    }
+void myFunctionFloat(float *f) {
+    printf("myFunctionFloat: %f\n", *f);
 }
 
-void printBottom(int height)
-{
-    for (int j = 0; j < height; j++)
-    {
-        for (int i = 0; i < width; i++)
-        {
-            printf("=");
-        }
-    }
+void myFunctionDefault(void *f) {
+    printf("myFunctionDefault %p\n", f);
 }
 
-void printScreen()
-{
-    printUp(2);
-    char literal[] = "hello world";
-    printSides(literal);
-    printBottom(2);
-}
-
-int main(int argc, char const *argv[])
-{
-    printScreen();
+int main() {
+    int a = 10;
+    myFunction(&a); // myFunctionInt
+    float b = 10.0;
+    myFunction(&b); // myFunctionFloat
+    double c = 10.00;
+    myFunction(&c); // myFunctionDefault
     return 0;
 }
