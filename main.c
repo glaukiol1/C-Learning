935c734f711f73d9bc714b68687085c9e59e831f
glaukiol1 <glaukiol1@gmail.com>
2022-04-23 21:37:51 +0200

    str length

--- main.c
+++ main.c
@@ -1,31 +1,11 @@
 // #include <stdlib.h>
 #include <stdio.h>
-// #include <string.h>
-
-#define myFunction(X) _Generic((X), \
-    int *:   myFunctionInt,         \
-    float *: myFunctionFloat,       \
-    default: myFunctionDefault      \
-)(X)
-
-void myFunctionInt(int *f) {
-    printf("myFunctionInt: %d\n", *f);
-}
-
-void myFunctionFloat(float *f) {
-    printf("myFunctionFloat: %f\n", *f);
-}
-
-void myFunctionDefault(void *f) {
-    printf("myFunctionDefault %p\n", f);
-}
+#include <string.h>
 
 int main() {
-    int a = 10;
-    myFunction(&a); // myFunctionInt
-    float b = 10.0;
-    myFunction(&b); // myFunctionFloat
-    double c = 10.00;
-    myFunction(&c); // myFunctionDefault
+    char * str;
+    gets(str);
+    int ln = strlen(str);
+    printf("Length of string: %d\n", ln);
     return 0;
 }
